4445d8f5447202d6d49b599f1d574c5ee51a91f7
lihao <1957512314@qq.com>
2017-09-30 21:03:40 +0800

    完成首页图表渲染，使用插件

--- public/js/config.js
+++ public/js/config.js
@@ -4,9 +4,11 @@ require.config({
     //借用的
     jquery: 'jquery/jquery.min',
     cookie:'jquery-cookie/jquery.cookie',
+    echarts:'echarts/echarts.min',
     //自己的
     common: '../js/common',
-    login: '../js/login'
+    login: '../js/login',
+    index:'../js/index'
   },
   shim: {}
 })
