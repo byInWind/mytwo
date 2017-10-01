b4f43f502d5b2115e3254dbc08235570de679436
lihao <1957512314@qq.com>
2017-10-01 21:29:18 +0800

    讲师的启用与注销

--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -2,16 +2,42 @@ define(['jquery', 'template', 'util'], function ($, template, util) {
   util.select('/teacher/list')
   //列表展示
   $.ajax({
-    url: '/api//teacher',
+    url: '/api/teacher',
     dataType: 'JSON',
     type: 'get',
     success: function (data) {
-      console.log(data)
       var html = template('tpl', {list: data.result});
       $("#teacherInfo").html(html);
+      //ajax是异步的，so展示成功后再做操作，不是并列关系
+      //注销启用
+
+      $('.startOrStop').click(function () {
+        var tcId = $(this).parent('td').attr('data-id');
+        var status = $(this).attr('data-status');
+        var that = this;
+        $.ajax({
+          url: '/api/teacher/handle',
+          dataType: 'JSON',
+          type: 'POST',
+          data: {tc_id: tcId, tc_status: status},
+          success: function (data) {
+            console.log(data)
+            if (data.code == 200) {
+              $(that).attr('data-status', data.result.tc_status);
+              if (data.result.tc_status == 1) {
+                $(that).text('启用')
+              } else {
+                $(that).text('注销')
+              }
+            }
+            //var html = template('tpl2', {list: data.result});
+            //$("#teacherInfo").html(html);
+          }
+        })
+      })
+
     }
   })
-  //注销启用
 
   //模态框
 
