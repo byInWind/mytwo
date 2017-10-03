a3f54425ccb11535c9bf3e707b3ae9f913e7ae4d
lihao <1957512314@qq.com>
2017-10-03 14:00:39 +0800

    完成创建课程

--- public/js/course-add.js
+++ public/js/course-add.js
@@ -1,4 +1,23 @@
-define(['jquery','template','util'],function ($,template,util) {
-  util.select('/course/add')
-
+define(['jquery', 'template', 'util', 'validate', 'form'], function ($, template, util) {
+  util.select('/course/add');
+  $("#creatInfo").validate({
+    sendForm: false,
+    valid: function () {
+      $(this).ajaxSubmit({
+        url: '/api/course/create',
+        type: 'POST',
+        dataType: 'json',
+        success: function (data) {
+          if (data.code == 200) {
+            location.href = '/course/base?cs_id=' + data.result.cs_id;
+          }
+        }
+      })
+    },
+    description: {
+      cs_name: {
+        required: "课程不能为空"
+      }
+    }
+  })
 })
